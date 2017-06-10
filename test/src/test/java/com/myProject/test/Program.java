f77c37c8c83e3e2fc5a5a7a06e7253e4d76b228f
janardhan <janardhankaragir@gmail.com>
2017-06-10 16:33:16 +0530

    second commit

---
+++ test/src/test/java/com/myProject/test/Program.java
@@ -0,0 +1,27 @@
+package com.myProject.test;
+
+import java.io.File;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Program {
+
+		@Test
+		
+		public void method() throws Exception{
+			
+			
+			String path = new File(".").getCanonicalPath()+ "//driver//chromedriver.exe";
+
+			System.setProperty("webdriver.chrome.driver", path);
+
+			WebDriver driver=new ChromeDriver();
+			driver.manage().window().maximize();
+			driver.get("https://www.google.co.in/?gfe_rd=cr&ei=7lg2WbqnJKrougTZ-oCgCg");
+				
+		}
+
+
+}

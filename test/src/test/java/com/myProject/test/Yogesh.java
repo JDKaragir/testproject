5c73479b1357eddce996f8bd3f51fcbb6ee86651
janardhan <janardhankaragir@gmail.com>
2017-06-11 11:57:12 +0530

    modified testing.xml

--- test/src/test/java/com/myProject/test/Yogesh.java
+++ test/src/test/java/com/myProject/test/Yogesh.java
@@ -1,5 +1,13 @@
 package com.myProject.test;
 
+import org.testng.annotations.Test;
+
 public class Yogesh {
+	@Test
+	
+	public void method() throws Exception{
+		
+		System.out.println("Welcome to java");
+	}
 
 }
